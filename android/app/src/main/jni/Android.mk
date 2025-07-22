9566dd358249b55cbf25ff15cf450a38d0a13a0c
rmchekzz <remcheckzz@gmail.com>
2025-07-22 21:13:24 +0700

    linked

---
+++ android/app/src/main/jni/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mcvoxel # these are all links below this comment :P
+LOCAL_SRC_FILES := \
+    ../../../../../../src/main.c \
+    ../../../../../../src/gameloop.c \
+    ../../../../../../src/gui.c \
+    ../../../../../../src/inputbuffer.c \
+    ../../../../../../src/menus.c \
+    ../../../../../../src/minecraftfont.c \
+    ../../../../../../src/options.c \
+    ../../../../../../src/player.c \
+    ../../../../../../src/terrain.c \
+    ../../../../../../src/textures.c \
+    ../../../../../../src/utility.c \
+    ../../../../../../src/data.c
+
+LOCAL_C_INCLUDES := ../../../../../../src 
+
+LOCAL_LDLIBS := -llog -landroid
+
+include $(BUILD_SHARED_LIBRARY)
